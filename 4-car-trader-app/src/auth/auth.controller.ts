27141f43b67b2b8a9ce34cb651d5b42098960a65
prasanv <prasan.venkatb@gmail.com>
2025-06-03 21:55:54 -0700

    added sign up new user as part of auth service

--- 4-car-trader-app/src/auth/auth.controller.ts
+++ 4-car-trader-app/src/auth/auth.controller.ts
@@ -2,12 +2,8 @@ import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UsersService } from "src/users/users.service";
 import { CreateUserDto } from "src/users/dto/create-user.dto";
-import { randomBytes, scrypt as _scrypt } from "crypto";
-import { promisify } from "util";
 import { ViewUserDto } from "src/users/dto/view-user.dto";
 import { Serialize } from "src/users/interceptor/serialize.intercept";
-
-const scrypt = promisify(_scrypt);
 
 @Controller("auth")
 export class authController {
@@ -22,27 +18,16 @@ export class authController {
     const { email, password } = body;
 
     // Check if the user email already if true throw error
-    const user = await this.usersService.findByEmail(email);
+    const userList = await this.usersService.findByEmail(email);
 
-    if (user.length) {
+    if (userList.length) {
       throw new BadRequestException("Email already exits");
     }
 
-    // Hashing new user password
-    // Generate a salt
-    const salt = randomBytes(8);
+    // Create a new user and return it
+    const user = await this.authService.createNewUser(email, password);
 
-    // Hash the salt and the password together
-    const hash = (await scrypt(password, salt, 32)) as Buffer<ArrayBufferLike>;
-
-    // Join the hashed result and the salt together
-    const hashedPassword = salt.toString("hex") + "." + hash.toString("hex");
-
-    // Create new user and save it
-    const newlyCreatedUser = await this.usersService.create(email, hashedPassword);
-
-    // return newly created user
-    return newlyCreatedUser;
+    return user;
   }
 
   login() {}
--- 4-car-trader-app/src/auth/auth.service.ts
+++ 4-car-trader-app/src/auth/auth.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {
+  constructor(private usersService: UsersService) {}
 
-    
+  async createNewUser(email: string, password: string) {
+    // Hashing new user password
+    // Generate a salt
+    const salt = randomBytes(8);
+
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer<ArrayBufferLike>;
+
+    // Join the hashed result and the salt together
+    const hashedPassword = salt.toString("hex") + "." + hash.toString("hex");
+
+    // Create new user and save it
+    const newlyCreatedUser = await this.usersService.create(email, hashedPassword);
+
+    // return newly created user
+    return newlyCreatedUser;
   }
 }
